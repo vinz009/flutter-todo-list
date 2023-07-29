7dc077850d31bfdc31fa26241b74cf002b8174e9
Vincent <vincentamoguis@gmail.com>
2023-07-29 16:40:42 +0800

    feat: Added Material Theme on the App

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,15 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const Todo());
-
-class Todo extends StatelessWidget {
-	const Todo();
-
-	@override
-	Widget build(BuildContext context) {
-		return Container(
-			decoration: const BoxDecoration(color: Colors.yellow),
-			child: const Center(
-				child: Text(
-				'hello World',
-				textDirection: TextDirection.ltr,
-				style: TextStyle(
-					fontSize: 32,
-					color: Colors.black,
-					),
+void main() {
+	runApp(
+		const MaterialApp(
+			title: 'Todo List',
+			home: Scaffold(
+				body: Center(
+					child: Text('hello world')
 				),
 			),
-		);
-	}
+		),
+	);
 }
+				
