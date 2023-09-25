92e4bb2b9af0591ea36ab307653928a3828e6782
Vincent <vincentamoguis@gmail.com>
2023-09-25 17:52:14 +0800

    refactor: Used Material widgets design

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() {
-	return runApp(
-		MaterialApp(
-			home: Scaffold(
-				appBar: AppBar(
-				title: Text('Todo List'),
-				),
-				body: Column(
-				children: [
-					Container(
-					child: Text('hi'),
-					),
-					],
-					),
-				),
-			),
-	);
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Todo List'),
+        ),
+        body: Column(
+          children: [
+						ListTile(
+							title: Text('To grocery store'),
+							tileColor: Colors.yellow,
+							trailing: Ink(
+								decoration: ShapeDecoration(
+								color: Colors.red,
+								shape: CircleBorder(),
+							),
+								child: Icon(
+								Icons.delete,
+								color: Colors.white,
+								),
+							),
+							),
+          ],
+        ),
+      ),
+    ),
+  );
 }
-				
