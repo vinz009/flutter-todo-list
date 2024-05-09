99c708a9d372294d2955ff7c07a5d2f465342e37
Vincent <vincentamoguis@gmail.com>
2024-05-09 17:30:13 +0800

    refactor: remove other platform other than android

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,44 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  return runApp(
-    MaterialApp(
+  return runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Todo List'),
         ),
         body: Column(
           children: [
-						ListTile(
-							title: Text('To grocery store'),
-							tileColor: Colors.yellow,
-							trailing: Ink(
-								decoration: ShapeDecoration(
-								color: Colors.red,
-								shape: CircleBorder(),
-							),
-								child: Icon(
-								Icons.delete,
-								color: Colors.white,
-								),
-							),
-							),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Add todo\'s',
+              ),
+            ),
+            ListTile(
+              title: Text('To grocery store'),
+              tileColor: Colors.yellow,
+              trailing: Ink(
+                decoration: ShapeDecoration(
+                  color: Colors.red,
+                  shape: CircleBorder(),
+                ),
+                child: Icon(
+                  Icons.delete,
+                  color: Colors.white,
+                ),
+              ),
+            ),
           ],
         ),
       ),
-    ),
-  );
+    );
+  }
 }
